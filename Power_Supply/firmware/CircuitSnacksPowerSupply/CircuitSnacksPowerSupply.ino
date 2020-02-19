c3b691533fe0b89b62643aeca266120596d7a382
gabebuckmaster <buckmaster.gabe@gmail.com>
2020-02-19 15:36:11 -0800

    Implemented a basic UI for performing calibration
    Implemented a basic UI for performing calibration

--- Power_Supply/firmware/CircuitSnacksPowerSupply/CircuitSnacksPowerSupply.ino
+++ Power_Supply/firmware/CircuitSnacksPowerSupply/CircuitSnacksPowerSupply.ino
@@ -23,6 +23,7 @@ void updateDisplay(float voltage_setpoint, float current_setpoint, float current
 void initStates();
 void updateJoystick();
 void updateMode();
+void performCalibration();
 
 float voltageAdjust; 
 float currentAdjust;
@@ -71,25 +72,7 @@ void setup(){
   // for testing calibration code. Hold center button on reset to activate
   if(!digitalRead(BUTTON_CENTER_N_PIN))
   {
-    // output must be open for this section
-    digitalWrite(USER_LED_PIN, LOW); // LED ON
-    ps.startVoltageCal();
-    delay(100);
-    // measured 10.01V
-    ps.finishVoltageCal(10.01);
-    ps.startCurrentCal();
-    delay(1000);
-    digitalWrite(USER_LED_PIN, HIGH); // LED OFF
-
-    // output must be shorted for this section, so wait for user to press button again
-    while(!digitalRead(BUTTON_CENTER_N_PIN)); // wait for button release
-    delay(100); //debounce
-    while(digitalRead(BUTTON_CENTER_N_PIN)); // wait for button press
-    
-    digitalWrite(USER_LED_PIN, LOW); // LED ON
-    // measured 81 mA
-    ps.finishCurrentCal(0.081);
-    digitalWrite(USER_LED_PIN, HIGH); // LED OFF
+    performCalibration();
   }
 }
 
@@ -252,5 +235,263 @@ void updateMode(){
   }else{
     u8g2.drawStr(DISPLAY_WIDTH/2-(u8g2.getStrWidth("off")/2), DISPLAY_HEIGHT/2+2,  "off");
   }
+}
 
+// must be called after the display is set up!
+void performCalibration()
+{
+  u8g2.setFontRefHeightExtendedText();
+  u8g2.setDrawColor(1);
+  u8g2.setFontPosTop();
+  u8g2.setFontDirection(0);
+
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_7x14_tf);
+  u8g2.drawStr(DISPLAY_WIDTH/2-u8g2.getStrWidth("CALIBRATION MODE")/2, DISPLAY_HEIGHT/2-14/2, "CALIBRATION MODE");
+  u8g2.sendBuffer();
+
+  while(!digitalRead(BUTTON_CENTER_N_PIN)); // wait for button release
+  delay(100); //debounce
+
+  ps.startVoltageCal(); // start voltage calibration procedure
+
+  float measured_voltage = 10.0;
+  int cursor_position = 0;
+  const int CURSOR_POSITION_MAX_VOLTS = 1;
+  const int CURSOR_POSITION_MIN_VOLTS = -2;
+  const float MEASURED_VOLTAGE_MAX = 20.0;
+  const float MEASURED_VOLTAGE_MIN = 0.0;
+  while(digitalRead(BUTTON_CENTER_N_PIN)) // press center button for next
+  {
+    // update the states of the joystick (except center)
+    up.curr = digitalRead(BUTTON_UP_N_PIN);
+    down.curr = digitalRead(BUTTON_DOWN_N_PIN);
+    right.curr = digitalRead(BUTTON_RIGHT_N_PIN);
+    left.curr = digitalRead(BUTTON_LEFT_N_PIN);
+
+    // handle moving cursor left and right
+    if(!left.curr && left.prev)
+    {
+      if (cursor_position < CURSOR_POSITION_MAX_VOLTS)
+      {
+        cursor_position++;
+      }
+    }
+    if(!right.curr && right.prev)
+    {
+      if (cursor_position > CURSOR_POSITION_MIN_VOLTS)
+      {
+        cursor_position--;
+      }
+    }
+
+    // handle incrementing and decrementing the digits
+    if(!up.curr && up.prev)
+    {
+      switch(cursor_position)
+      {
+        case 1:
+          if (measured_voltage + 10.0 <= MEASURED_VOLTAGE_MAX)
+          {
+            measured_voltage += 10.0;
+          }
+          break;  
+        case 0:
+          if (measured_voltage + 1.0 <= MEASURED_VOLTAGE_MAX)
+          {
+            measured_voltage += 1.0;
+          }
+          break;  
+        case -1:
+          if (measured_voltage + 0.1 <= MEASURED_VOLTAGE_MAX)
+          {
+            measured_voltage += 0.1;
+          }
+          break;  
+        case -2:
+          if (measured_voltage + 0.01 <= MEASURED_VOLTAGE_MAX)
+          {
+            measured_voltage += 0.01;
+          }
+          break;
+      }
+    }
+    if(!down.curr && down.prev)
+    {
+      switch(cursor_position)
+      {
+        case 1:
+          if (measured_voltage - 10.0 >= MEASURED_VOLTAGE_MIN)
+          {
+            measured_voltage -= 10.0;
+          }
+          break;  
+        case 0:
+          if (measured_voltage - 1.0 >= MEASURED_VOLTAGE_MIN)
+          {
+            measured_voltage -= 1.0;
+          }
+          break;  
+        case -1:
+          if (measured_voltage - 0.1 >= MEASURED_VOLTAGE_MIN)
+          {
+            measured_voltage -= 0.1;
+          }
+          break;  
+        case -2:
+          if (measured_voltage - 0.01 >= MEASURED_VOLTAGE_MIN)
+          {
+            measured_voltage -= 0.01;
+          }
+          break;
+      }
+    }
+
+    // current states become previous states
+    up.prev = up.curr;
+    down.prev = down.curr;
+    right.prev = right.curr;
+    left.prev = left.curr;
+
+    // update display
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_7x14_tf);
+    u8g2.drawStr(DISPLAY_WIDTH/2-u8g2.getStrWidth("CALIBRATION MODE")/2, 2, "CALIBRATION MODE");
+    u8g2.drawStr(10, 14, "Measure output");
+    u8g2.drawStr(10, 26, "voltage:"); 
+    sprintf(string_buffer, "%.2d.%.2d V ",(uint32_t) (measured_voltage), ((uint32_t) (measured_voltage*100.0))%100); 
+    u8g2.drawStr(DISPLAY_WIDTH/2-u8g2.getStrWidth(string_buffer)/2, 48, string_buffer);
+    if(cursor_position >= 0) //we're before the decimal point
+    {
+      u8g2.drawLine(DISPLAY_WIDTH/2-u8g2.getStrWidth(string_buffer)/2+(CURSOR_POSITION_MAX_VOLTS-cursor_position)*7, 62, 
+              DISPLAY_WIDTH/2-u8g2.getStrWidth(string_buffer)/2+(CURSOR_POSITION_MAX_VOLTS-cursor_position)*7+6, 62); 
+    }
+    else // we're after the decimal point
+    {
+      u8g2.drawLine(DISPLAY_WIDTH/2-u8g2.getStrWidth(string_buffer)/2+(CURSOR_POSITION_MAX_VOLTS-cursor_position)*7+7, 62, 
+              DISPLAY_WIDTH/2-u8g2.getStrWidth(string_buffer)/2+(CURSOR_POSITION_MAX_VOLTS-cursor_position)*7+7+6, 62); 
+    }
+    u8g2.sendBuffer();
+  }
+
+  // draw "Wait..."
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_7x14_tf);
+  u8g2.drawStr(DISPLAY_WIDTH/2-u8g2.getStrWidth("CALIBRATION MODE")/2, 2, "CALIBRATION MODE");
+  u8g2.drawStr(DISPLAY_WIDTH/2-u8g2.getStrWidth("Wait...")/2, DISPLAY_HEIGHT/2-7, "Wait...");
+  u8g2.sendBuffer();
+  
+  ps.finishVoltageCal(measured_voltage); // finish voltage cal with measured value
+
+  ps.startCurrentCal(); // start current cal
+
+  float measured_current = 0.1;
+  cursor_position = 0;
+  const int CURSOR_POSITION_MAX_CURRENT_MA = 2;
+  const int CURSOR_POSITION_MIN_CURRENT_MA = 0;
+  const float MEASURED_CURRENT_MAX = 0.2;
+  const float MEASURED_CURRENT_MIN = 0.0;
+  while(digitalRead(BUTTON_CENTER_N_PIN)) // press center button for next
+  {
+    // update the states of the joystick (except center)
+    up.curr = digitalRead(BUTTON_UP_N_PIN);
+    down.curr = digitalRead(BUTTON_DOWN_N_PIN);
+    right.curr = digitalRead(BUTTON_RIGHT_N_PIN);
+    left.curr = digitalRead(BUTTON_LEFT_N_PIN);
+
+    // handle moving cursor left and right
+    if(!left.curr && left.prev)
+    {
+      if (cursor_position < CURSOR_POSITION_MAX_CURRENT_MA)
+      {
+        cursor_position++;
+      }
+    }
+    if(!right.curr && right.prev)
+    {
+      if (cursor_position > CURSOR_POSITION_MIN_CURRENT_MA)
+      {
+        cursor_position--;
+      }
+    }
+
+    // handle incrementing and decrementing the digits
+    if(!up.curr && up.prev)
+    {
+      switch(cursor_position)
+      {
+        case 2:
+          if (measured_current + 0.1 <= MEASURED_CURRENT_MAX)
+          {
+            measured_current += 0.1;
+          }
+          break; 
+        case 1:
+          if (measured_current + 0.01 <= MEASURED_CURRENT_MAX)
+          {
+            measured_current += 0.01;
+          }
+          break;  
+        case 0:
+          if (measured_current + 0.001 <= MEASURED_CURRENT_MAX)
+          {
+            measured_current += 0.001;
+          }
+          break;  
+      }
+    }
+    if(!down.curr && down.prev)
+    {
+      switch(cursor_position)
+      {
+        case 2:
+          if (measured_current - 0.1 >= MEASURED_CURRENT_MIN)
+          {
+            measured_current -= 0.1;
+          }
+          break; 
+        case 1:
+          if (measured_current - 0.01 >= MEASURED_CURRENT_MIN)
+          {
+            measured_current -= 0.01;
+          }
+          break;  
+        case 0:
+          if (measured_current - 0.001 >= MEASURED_CURRENT_MIN)
+          {
+            measured_current -= 0.001;
+          }
+          break;  
+      }
+    }
+
+    // current states become previous states
+    up.prev = up.curr;
+    down.prev = down.curr;
+    right.prev = right.curr;
+    left.prev = left.curr;
+
+    // update display
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_7x14_tf);
+    u8g2.drawStr(DISPLAY_WIDTH/2-u8g2.getStrWidth("CALIBRATION MODE")/2, 2, "CALIBRATION MODE");
+    u8g2.drawStr(10, 14, "Measure output");
+    u8g2.drawStr(10, 26, "shorted current:"); 
+    sprintf(string_buffer, "%.3d mA ",(uint32_t) (measured_current*1000)); 
+    u8g2.drawStr(DISPLAY_WIDTH/2-u8g2.getStrWidth(string_buffer)/2, 48, string_buffer);
+
+    u8g2.drawLine(DISPLAY_WIDTH/2-u8g2.getStrWidth(string_buffer)/2+(CURSOR_POSITION_MAX_CURRENT_MA-cursor_position)*7, 62, 
+                  DISPLAY_WIDTH/2-u8g2.getStrWidth(string_buffer)/2+(CURSOR_POSITION_MAX_CURRENT_MA-cursor_position)*7+6, 62); 
+    
+    u8g2.sendBuffer();
+  }
+
+  // draw "Wait..."
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_7x14_tf);
+  u8g2.drawStr(DISPLAY_WIDTH/2-u8g2.getStrWidth("CALIBRATION MODE")/2, 2, "CALIBRATION MODE");
+  u8g2.drawStr(DISPLAY_WIDTH/2-u8g2.getStrWidth("Wait...")/2, DISPLAY_HEIGHT/2-7, "Wait...");
+  u8g2.sendBuffer();
+  
+  ps.finishCurrentCal(measured_current); // finish current cal with measured value (saves to NVM)
 }
